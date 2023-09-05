377e5e41cab4f3ecb12fc03bde915ca1047b1d29
Lebohang1821 <millboylebohang@gmail.com>
2023-09-05 13:10:13 -0700

    task-100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,5 @@
 
 # Print the alphabet in reverse order alternating upper- and lower-case.
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c) if (ord('z') - c) % 2 == 0 else chr(c).upper()), end="")
+    print("{}".format(chr(c) if (ord('z') - c) % 2 == 0 else chr(c).upper()),
+            end="")
