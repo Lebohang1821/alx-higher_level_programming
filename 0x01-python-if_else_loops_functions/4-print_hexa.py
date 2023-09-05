c18084d7a3736a6962db7fd30f129e55933ced49
Lebohang1821 <millboylebohang@gmail.com>
2023-09-05 08:54:04 -0700

    task-4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-#4-print_hexa.py
+#!/usr/bin/python3
+# 4-print_hexa.py
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
-    print(f"{number} = {hex(number)}")
+    print("{:d} = 0x{:02X}".format(number, number))
