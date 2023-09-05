9d9342849296851d82073e8b012c62289f4385ce
Lebohang1821 <millboylebohang@gmail.com>
2023-09-05 08:37:27 -0700

    task-2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
-for ascii_code in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_code), end='')
+#!/usr/bin/python3
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
