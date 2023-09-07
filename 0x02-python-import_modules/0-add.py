204979a86d1c83b559c1686ca4090ab174a8aab1
Lebohang1821 <millboylebohang@gmail.com>
2023-09-07 12:25:16 -0700

    task-0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,10 +4,7 @@ def main():
     """Print the sum of 1 and 2."""
     from add_0 import add
 
-    a = 1
-    b = 2
+    a, b = 1, 2
     result = add(a, b)
     print(f"{a} + {b} = {result}")
 
-if __name__ == "__main__":
-    main()
