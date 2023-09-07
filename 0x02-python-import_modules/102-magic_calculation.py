4a5f8664043abac0188a65e09dfd49fcf91473ff
Lebohang1821 <millboylebohang@gmail.com>
2023-09-07 13:41:02 -0700

    task-102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+    """Perform a custom magic calculation based on the values of 'a' and 'b'.
+    
+    If 'a' is less than 'b', it adds 'a' and 'b' and then adds numbers from 4 to 5.
+    Otherwise, it subtracts 'b' from 'a'.
+
+    :param a: An integer value.
+    :param b: An integer value.
+    :return: The result of the magic calculation.
+    """
+    from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
+        return (c)
+
     else:
         return sub(a, b)
