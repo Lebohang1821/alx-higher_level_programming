8932f8249033386a31a14d67bd08f77bb8b189f8
Lebohang1821 <millboylebohang@gmail.com>
2023-09-10 09:41:28 -0700

    task-2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replace an element of a list at a specific position.
+    """
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return my_list
