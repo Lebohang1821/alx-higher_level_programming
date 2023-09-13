92886cddd2c06a839fec0f5fed4529d82cfe4eed
Lebohang1821 <millboylebohang@gmail.com>
2023-09-13 17:05:30 +0000

    task-100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 def weight_average(my_list):
-    # Check if the list is empty
     if not my_list:
         return 0
 
     weighted_sum = 0
     total_weight = 0
 
-    # Calculate the weighted sum and total weight
     for (value, weight) in my_list:
         weighted_sum += value * weight
         total_weight += weight
 
-    # Calculate and return the weighted average
     if total_weight == 0:
         return 0
     else:
