13a67a75ac4b60fa9ec4d413875399966ccb4e41
Lebohang1821 <millboylebohang@gmail.com>
2023-09-24 14:16:26 +0000

    task-1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    """
-	Print integer with "{:d}".format().
+    """Print integer with "{:d}".format().
 
     Args:
         value (int): Integer print.
