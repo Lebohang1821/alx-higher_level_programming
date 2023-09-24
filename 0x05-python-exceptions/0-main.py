8ed2648403322183a545a69e5dee40cc2c1f8524
Lebohang1821 <millboylebohang@gmail.com>
2023-09-24 13:28:11 +0000

    task-0

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,4 +1,5 @@
-afe_print_list = __import__('0-safe_print_list').safe_print_list
+#!/usr/bin/python3
+safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
 
---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    """
+	Prints x elememts of list.
+
+    Args:
+        my_list (list): List to print elements from.
+        x (int): Num of elements of my_list - print.
+
+    Returns:
+        Num of elements printed.
+    """
+    the_ret = 0
+    for l in range(x):
+        try:
+            print("{}".format(my_list[l]), end="")
+            the_ret += 1
+        except IndexError:
+            break
+    print("")
+    return (the_ret)
