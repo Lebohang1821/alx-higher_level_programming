025f834f12b27b2e51930e161d334dbb5963f4ea
Lebohang1821 <millboylebohang@gmail.com>
2023-10-04 21:05:56 +0000

    task-0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,8 @@
 def add_integer(a, b=98):
     """It returns integer addition of a and b.
 
-    Float arguments are typecasted - ints before addition performed.
+    Float arguments are typecasted - 
+    ints before addition performed.
 
     Raises:
         TypeError: If a or b is non-integer and non-float.
@@ -15,4 +16,3 @@ def add_integer(a, b=98):
     if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
