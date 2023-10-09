6ffbdb971ea3377dd54b10c25bafd3daeadfb780
Lebohang1821 <millboylebohang@gmail.com>
2023-10-09 21:36:32 +0000

    task-0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-"""
-Object Attribute Lookup Function
-"""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """
-    Get list of attributes of an object.
-
-    Args:
-        obj (object): The Python object for which attributes are to be retrieved.
-
-    Returns:
-        list: A list containing the names of all attributes associated with the object.
-    """
-    # Use the 'dir()' function to obtain a list of attributes for the given object.
+    """Return a list of an object's available attributes."""
     return (dir(obj))
