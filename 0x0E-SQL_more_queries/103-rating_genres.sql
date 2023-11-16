6593f3ab29ca014664888e3be22b1bc33aba1996
Lebohang1821 <millboylebohang@gmail.com>
2023-11-16 11:14:49 -0800

    task-103

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,12 @@
+-- It dis all genres in database 
+-- hbtn_0d_tvshows_rate by their rating
+
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
