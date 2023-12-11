178cae7c190e55964cfb9a7f7c3c854775419792
Lebohang1821 <millboylebohang@gmail.com>
2023-12-11 03:32:29 -0800

    task-3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
+
 // It print first argument passed or "No argument" if none
 
-// It gets first argument passed to script
-const firstArgument = process.argv[2];
-
-// Console.log(...) to print output
-if (firstArgument) {
-  console.log(firstArgument);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-  console.log("No argument");
+  console.log(process.argv[2]);
 }
