6d43667948c677e6325b7e18c86734d02806a594
Lebohang1821 <millboylebohang@gmail.com>
2023-12-11 03:28:53 -0800

    task-1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,10 +2,9 @@
 // It print multiple lines using console.log
 
 // First line: “C is fun”
-console.log("C is fun");
-
-// Second line: “Python is cool”
-console.log("Python is cool");
-
-//Third line: “JavaScript is amazing”
-console.log("JavaScript is amazing");
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
+console.log(c);
+console.log(python);
+console.log(javascript);
