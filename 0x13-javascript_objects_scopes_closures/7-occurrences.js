a9ae305bdb62c24adbfc2e091b0bf129b2c405a0
Lebohang1821 <millboylebohang@gmail.com>
2023-12-12 12:55:06 -0800

    task-7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 // It returns the number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
-  for (let x = 0; x < list.length; x++) {
+  for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       nOccurrences++;
     }
