8310c679f1d2557402249eabba2e1fccac188c1b
Lebohang1821 <millboylebohang@gmail.com>
2023-12-12 12:30:21 -0800

    task-4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// It defines a rectangle:
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let s = '';
+      for (let l = 0; l < this.width; l++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+    rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
