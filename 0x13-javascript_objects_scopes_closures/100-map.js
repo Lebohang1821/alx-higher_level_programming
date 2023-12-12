472e022be4c9a56d8f305fc74a442118b3a29095
Lebohang1821 <millboylebohang@gmail.com>
2023-12-12 13:13:54 -0800

    task-100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // It imports an array and computes a new array.
 const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
+const newList = list.map(function (numb, index) {
+  return numb * index;
 });
 
 console.log(list);
