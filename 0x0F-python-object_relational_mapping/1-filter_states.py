144af00fc343f4244952f7c8b1993ff17ca6f0a4
Lebohang1821 <millboylebohang@gmail.com>
2023-12-16 18:20:34 +0000

    task-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-"Startin point"
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
