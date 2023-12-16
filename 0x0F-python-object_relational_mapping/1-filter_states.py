87a9f7107cf96caec3f58b5c06d7b42be9c973a6
Lebohang1821 <millboylebohang@gmail.com>
2023-12-16 18:32:17 +0000

    task-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,28 +4,11 @@ It lists all states with name starting with N from database hbtn_0e_0_usa
 mysql username, mysql password and database name
 module MySQLdb (import MySQLdb)
 """
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(user=username, passwd=password, db=database)
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`")
-        states_starting_with_n = cursor.fetchall()
-
-        for state in states_starting_with_n:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-
-    finally:
-        cursor.close()
-        db.close()
+    bd = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], bd=sys.argv[3])
+    c = bd.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
