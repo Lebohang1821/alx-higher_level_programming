f819cd4f1853d9ee1823315fb56c5c1744ffbe08
Lebohang1821 <millboylebohang@gmail.com>
2023-12-16 18:14:59 +0000

    task-0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-#It check if correct number of command-line arguments is provided
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
