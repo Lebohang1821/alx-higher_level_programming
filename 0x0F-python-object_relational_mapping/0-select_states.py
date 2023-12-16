9c2e2c5155055aa423ab6276541e2e4f8356ad46
Lebohang1821 <millboylebohang@gmail.com>
2023-12-16 18:12:09 +0000

    task-0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,12 @@
 It Lists * states from database hbtn_0e_0_usa.
 mysql username, mysql password and database name
 """
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
 # It check if correct number of command-line arguments is provided
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-# It close cursor and connection
     c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall()]
