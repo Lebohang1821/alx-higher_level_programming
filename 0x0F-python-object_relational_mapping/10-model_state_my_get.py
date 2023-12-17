a6153ae8d3f2fd262f179ab65e1142263fcf48c3
Lebohang1821 <millboylebohang@gmail.com>
2023-12-17 06:45:23 +0000

    task-10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Lists the State object with the name passed as argument
-from the database hbtn_0e_6_usa.
-Usage: ./10-model_state_my_get.py <mysql username> /
-                                  <mysql password> /
-                                  <database name>
-                                  <state name searched>
+It prints State object with name passed as 
+-argument from database hbtn_0e_6_usa
+--4 arguments: mysql username, mysql password, 
+database name and state name - search (SQL injection free)
+---It display Not found
 """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
