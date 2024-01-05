d7b2c43146eec376dfbdb1aa1b0d73da31aecca7
Lebohang1821 <millboylebohang@gmail.com>
2024-01-05 07:33:10 -0800

    task-3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # It takes in URL and displays all HTTP methods server will accept
-url=$1; curl -sI $url | grep -i Allow | cut -d' ' -f2-
+curl -sI $url | grep -i Allow | cut -d' ' -f2-
 
