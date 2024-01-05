fb66f3854a98f995c7abdace027c2dbe46aa816c
Lebohang1821 <millboylebohang@gmail.com>
2024-01-05 07:57:12 -0800

    task-100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # It sends request - URL passed as argument, and displays only tatus code of response
-curl -s -o /dev/null -w '%{http_code}' '$1'
+curl -s -o /dev/null -w "%{http_code}" "$1"
