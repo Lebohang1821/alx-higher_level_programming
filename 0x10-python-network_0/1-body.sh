d933e1111968da15f7f1d4a28084519493fbb4a6
Lebohang1821 <millboylebohang@gmail.com>
2024-01-05 07:10:56 -0800

    tast-1 fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # It takes in URL, sends GET request to URL, and displays body of response
-curl -sfL "$1" -X GET | ( [ "$(curl -sw '%{http_code}' "$1")" == "200" ] && cat )
+curl -sfL "$1" -X GET
