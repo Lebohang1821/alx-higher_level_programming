fc32411726043586b5ee619be7a3895110f579d6
Lebohang1821 <millboylebohang@gmail.com>
2024-01-07 08:08:36 -0800

    task-0

--- 0-hbtn_status.py
+++ 0-hbtn_status.py
@@ -9,9 +9,11 @@ url = "https://alx-intranet.hbtn.io/status"
 
 try:
     with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
+        body = response.read()
+        utf8_content = body.decode('utf-8')
         print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body)
+        print("\t- utf8 content:", utf8_content)
 except urllib.error.URLError as e:
     print("Error:", e)
