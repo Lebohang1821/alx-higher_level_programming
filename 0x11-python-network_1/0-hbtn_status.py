23f8dc3abc59875e0bf1dd9d5dc4f5da6ee65213
Lebohang1821 <millboylebohang@gmail.com>
2024-01-07 08:30:34 -0800

    task-0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Its Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", utf8_content)
+except urllib.error.URLError as e:
+    print("Error:", e)
