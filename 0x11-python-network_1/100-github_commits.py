ebecdb65931176b7ae40a63847a4d874c7b7a17b
Lebohang1821 <millboylebohang@gmail.com>
2024-01-08 03:16:54 +0000

    task-100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""A script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
 """
+It takes 2 arguments in order to solve this challenge
+The first argument will be repository name
+second argument will be owner name
+"""
+
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
-def fetch_commits(repository, owner):
-    base_url = f"https://api.github.com/repos/{owner}/{repository}/commits"
-    response = requests.get(base_url)
-    commits = response.json()
-
-    for commit in commits[:10]:
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
-        print(f"{sha}: {author_name}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./script_name.py <repository_name> <owner_name>")
-        sys.exit(1)
-
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    fetch_commits(repository_name, owner_name)
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
