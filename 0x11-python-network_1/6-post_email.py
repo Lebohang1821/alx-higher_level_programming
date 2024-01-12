f98c099a2c6d54cfb6e3e44ebe58736e47f41381
Lebohang1821 <millboylebohang@gmail.com>
2024-01-11 17:17:23 -0800

    task-6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
+'''
 It takes in a URL and an email address
 sends POST request to passed URL with email as parameter
 Finally displays body of response.
-"""
-import requests
+'''
 import sys
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
 
-    print("Your email is:", email)
-
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
-
-    print(response.text)
+    r = requests.post(url, data=value)
+    print(r.text)
