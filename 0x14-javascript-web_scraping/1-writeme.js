e21a47c9737e058ae2d19c2ac147bd681176da1c
Lebohang1821 <millboylebohang@gmail.com>
2024-01-30 15:23:22 +0000

    task-1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const myfile = process.argv[2];
-const content = process.argv[3];
+const string = process.argv[3];
 
-if (!myfile || !content) {
-  console.error('Usage: ./1-writeme.js <file-path> <content>');
-  process.exit(1);
-}
-
-fs.writeFile(myfile, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Content written to ${myfile}`);
-  }
+fs.writeFile(myfile, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
