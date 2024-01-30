28d30f5f75ce2816c4495127ac0b173cd60b6323
Lebohang1821 <millboylebohang@gmail.com>
2024-01-30 15:13:17 +0000

    task-0' readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
