b31459ceeb649e0d101c8158f01a00719913f480
Lebohang1821 <millboylebohang@gmail.com>
2024-01-30 15:31:07 +0000

    task-2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const my_request = require('my_request');
+const request = require('request');
 
 const URL = process.argv[2];
 
-my_request(URL, function (err, response) {
+request(URL, function (err, response) {
   if (err) {
     console.log(err);
   } else {
