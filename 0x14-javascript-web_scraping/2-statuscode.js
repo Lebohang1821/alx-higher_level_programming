3b91e42fd73b9902198330f92756e47e24055b2d
Lebohang1821 <millboylebohang@gmail.com>
2024-01-30 15:33:02 +0000

    task-2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,26 @@
 #!/usr/bin/node
 
+// Import the 'request' module for making HTTP requests
 const request = require('request');
 
+// Get the URL from the command line arguments
 const URL = process.argv[2];
 
+// Check if a URL is provided
+if (!URL) {
+  // If not, print a usage message and exit with an error code
+  console.error('Usage: ./2-statuscode.js <URL>');
+  process.exit(1);
+}
+
+// Make a GET request to the specified URL
 request(URL, function (err, response) {
+  // Check if an error occurred during the request
   if (err) {
+    // If yes, print the error
     console.log(err);
   } else {
+    // If no error, print the status code from the response
     console.log('code: ' + response.statusCode);
   }
 });
