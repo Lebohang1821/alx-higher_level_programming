24907f1d0d62b08ec3d241984404df730eb6ea3e
Lebohang1821 <millboylebohang@gmail.com>
2024-02-08 06:17:50 -0800

    corrections

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
-    """
-    A simple command-line calculator for basic arithmetic operations.
-    """
-    if len(sys.argv) != 4:
+    """It manages basic arithmetic operations"""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if operator == "+":
-        result = a + b
-    elif operator == "-":
-        result = a - b
-    elif operator == "*":
-        result = a * b
-    elif operator == "/":
-        if b == 0:
-            print("Error: Division by zero is not allowed.")
-            sys.exit(1)
-        result = a / b
-    else:
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-(lambda x: x("#pythoniscool\n"))(__import__("os").write)
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
