2b0bab830755310e0bd74294325de5d97cf9f384
Lebohang1821 <millboylebohang@gmail.com>
2024-03-12 08:50:51 -0700

    fix

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+//  function incr that increments integer value
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
