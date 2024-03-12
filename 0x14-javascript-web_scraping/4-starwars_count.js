132a4d08eb909fadd4ce8c1f3f9b9b0f48fece62
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-03-12 18:25:09 +0200

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,34 +1,25 @@
 #!/usr/bin/node
 
-const request = require('request');
-
 // Get the API URL from the command line arguments
-const apiUrl = process.argv[2];
+const request = require('request');
+const url = process.argv[2];
 
-if (!apiUrl) {
-  console.error('Usage: ./4-starwars_count.js <API-URL>');
-  process.exit(1);
-}
-
-// Make a GET request to the Star Wars API
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    // Parse the JSON response
-    const filmsData = JSON.parse(body);
-
-    // Check if the films data is available
-    if (filmsData.results) {
-      // Filter films where "Wedge Antilles" is present
-      const filmsWithWedge = filmsData.results.filter((film) =>
-        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-      );
-
-      // Print the number of films with "Wedge Antilles"
-      console.log(filmsWithWedge.length);
-    } else {
-      console.log('Unable to fetch films data');
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
     }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
